a892757f70d195d5cd796490366cdac0fcecf966
Alistair Hutchison <alistair.hutchison@wildfireapp.com>
2012-04-04 18:15:10 +0100

    Added feature for Basic Info (incomplete)

---
+++ features/step_definitions/account_management/basic_info_steps.rb
@@ -0,0 +1,80 @@
+Given /^I have uploaded a company logo$/ do
+  @wildfire.account_management.basic_info.upload_logo("images/logo-wildfire-lrg.jpg")
+  @wildfire.account_management.basic_info.should_not(have_no_logo, "Company logo failed to upload successfully")
+end
+
+When /^I update my Company Name to "([^"]*)"$/ do |new_name|
+  @wildfire.account_management.basic_info.update_company_name(new_name)
+end
+
+When /^I update my Company Industry to "([^"]*)"$/ do |new_industry|
+  @wildfire.account_management.basic_info.update_company_industry(new_industry)
+end
+
+When /^I update my Company Timezone to "([^"]*)"$/ do |timezone|
+  @wildfire.account_management.basic_info.update_timezone(timezone)
+end
+
+When /^I update my Company website URL to "([^"]*)"$/ do |url|
+  @wildfire.account_management.basic_info.update_website_url(url)
+end
+
+When /^I update my Company Description to$/ do |description|
+  @wildfire.account_management.basic_info.update_description(description)
+end
+
+When /^I update my Company Email to "([^"]*)"$/ do |email|
+  @wildfire.account_management.basic_info.update_company_email(email)
+end
+
+When /^I update my Reply Email to "([^"]*)"$/ do |email|
+  @wildfire.account_management.basic_info.update_reply_email(email)
+end
+
+When /^I attempt to delete the company$/ do
+  @wildfire.account_management.basic_info.delete_company
+end
+
+When /^I upload a valid image as my company logo$/ do
+  @wildfire.account_management.basic_info.upload_logo("images/logo-wildfire-lrg.jpg")
+end
+
+When /^I remove the company logo$/ do
+  @wildfire.account_management.basic_info.remove_logo
+end
+
+Then /^the Company Name should be "([^"]*)"$/ do |name|
+  @wildfire.account_management.basic_info.company_name.should == name
+end
+
+Then /^the Company Industry should be "([^"]*)"$/ do |industry|
+  @wildfire.account_management.basic_info.industry.should == industry
+end
+
+Then /^the Company Timezone should be "([^"]*)"$/ do |timezone|
+  @wildfire.account_management.basic_info.timezone.should == timezone
+end
+
+Then /^the Company Website URL should be "([^"]*)"$/ do |url|
+  @wildfire.account_management.basic_info.website_url.should == url
+end
+
+Then /^the Company Description should be$/ do |description|
+  @wildfire.account_management.basic_info.description.should == description
+end
+
+Then /^the Company Email should be "([^"]*)"$/ do |email|
+  @wildfire.account_management.basic_info.company_email.should == email
+end
+
+Then /^the Reply Email should be "([^"]*)"$/ do |email|
+  @wildfire.account_management.basic_info.reply_email.should == email
+end
+
+Then /^I should no longer see the default logo image$/ do
+  @wildfire.account_management.basic_info.should_not(have_no_logo, "Expected not to find the default 'No logo' image, but it was present")
+end
+
+Then /^I should see the default logo image$/ do
+   @wildfire.account_management.basic_info.should(have_no_logo, "Expected to find the default 'No logo' image, but it was not present")
+end
---
+++ lib/model/page/account_management/basic_info.rb
@@ -0,0 +1,80 @@
+class Model::Page::AccountManagement::BasicInfo < SitePrism::Page
+  section :sidebar, Model::Section::AccountManagement::Sidebar, 'div.sidebar'
+  section :company_form, Model::Section::AccountManagement::CompanyForm, 'form#company_form'
+  section :company_logo_form, Model::Section::AccountManagement::CompanyLogoForm, 'form#upload_form'
+  
+  def update_company_name(name)
+    company_form.update_company_name(name)
+  end
+  
+  def update_company_industry(industry)
+    company_form.update_industry(industry)
+  end
+  
+  def update_timezone(timezone)
+    company_form.update_timezone(timezone)
+  end
+  
+  def update_website_url(url)
+    company_form.update_website_url(url)
+  end
+  
+  def update_description(description)
+    company_form.update_description(description)
+  end
+  
+  def update_company_email(email)
+    company_form.update_company_email(email)
+  end
+  
+  def update_reply_email(email)
+    company_form.update_reply_email(email)
+  end
+  
+  def delete_company
+    click_on('Delete Company')
+    page.accept_alert
+  end
+  
+  def upload_logo(filename)
+    company_logo_form.attach_file(filename)
+    company_logo_form.upload_button.click
+  end
+  
+  def remove_logo
+    company_logo_form.remove_button.click
+    page.accept_alert
+  end
+  
+  def company_name
+    find_field('Company Name').value
+  end
+  
+  def industry
+    find_field('Industry').find('option[selected]').text
+  end
+  
+  def timezone
+    find_field('Timezone').find('option[selected]').text
+  end
+  
+  def website_url
+    find_field('Website URL').value
+  end
+  
+  def description
+    find_field('Description').value
+  end
+  
+  def company_email
+    find_field('Company Email').value
+  end
+  
+  def reply_email
+    find_field('Reply Email').value
+  end
+  
+  def has_no_logo?
+    first("img[alt='Nologo']") ? true : false
+  end
+end
