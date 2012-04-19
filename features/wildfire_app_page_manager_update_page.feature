e155caab9b9fed0317e84ff220ecc7cd3c733d82
Nathan Bain <nathan_bain@hotmail.com>
2012-04-19 13:13:38 +0100

    simplifying wildfire_countdown_template_edit_header to remove unnescessary checks and waits

--- features/wildfire_app_page_manager_update_page.feature
+++ features/wildfire_app_page_manager_update_page.feature
@@ -5,7 +5,7 @@ Feature: Update Page
    Given I am logged in as "default"
    And I navigate to the wildfire app page manager page
 
-  @complete
+  @complete @production
   Scenario: Publish an update to an App in Page Manager
     When I update and publish the Countdown App
     And I navigate to the palo alto foodies countdown app page
--- lib/model/page/wildfireapp_countdown_template_edit_header.rb
+++ lib/model/page/wildfireapp_countdown_template_edit_header.rb
@@ -2,32 +2,16 @@ class Model::Page::WildfireappCountdownTemplateEditHeader < SitePrism::Page
   element :save_button, 'button#internal_widget_submit'
 
   def body_text(text)
+    script_executed = false
     attempts = 0
-    while attempts < 30 && !body_contains?(text)
+    while !script_executed && attempts < 30
+      attempts = attempts + 1
       begin
-        attempts += 1
-        set_body_text(text)
-      rescue Exception => e
+        page.execute_script "tinymce.get('internal_widget_body').setContent('#{text}')"
+        script_executed = true
+      rescue
+        # page not rendered yet - wait a while
         sleep 0.5
-        # Keep trying until timeout is reached
-      end
-    end
-  end
-
-  def set_body_text(text)
-    page.execute_script "tinymce.get('internal_widget_body').setContent('#{text}')"
-  end
-
-  def body_contains?(text)
-    attempts = 0
-    got_text = false
-    while attempts < 30 && !got_text
-      begin
-        attempts += 1
-        got_text = page.html != nil
-      rescue Exception => e
-        sleep 0.5
-        # Keep trying until timeout is reached
       end
     end
   end
