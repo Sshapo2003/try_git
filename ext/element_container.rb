99d3a2af19861bde4a15e5cefc4a5187a9f29ac5
Nathan Bain <nathan_bain@hotmail.com>
2012-05-16 12:49:55 -0700

    Adding monkeypatched element_container

---
+++ ext/element_container.rb
@@ -0,0 +1,60 @@
+module SitePrism::ElementContainer
+
+  def root_element element_name, element_locator = nil
+    if element_locator.nil?
+      define_method element_name.to_s do
+        raise SitePrism::NoLocatorForElement.new("#{self.class.name} => :#{element_name} needs a locator")
+      end
+    else
+      add_element_name element_name.to_s
+      define_method element_name.to_s do
+        find locator
+      end
+    end
+    create_existence_checker element_name, element_locator
+    create_waiter element_name, element_locator
+  end
+
+  def root_elements collection_name, collection_locator = nil
+    if collection_locator.nil?
+      define_method collection_name.to_s do
+        raise SitePrism::NoLocatorForElement.new("#{self.class.name} => :#{element_name} needs a locator")
+      end
+    else
+      add_element_name collection_name
+      define_method collection_name.to_s do
+        all locator
+      end
+    end
+    create_existence_checker collection_name, collection_locator
+    create_waiter collection_name, collection_locator
+  end
+  alias :collection :elements
+
+  def root_section section_name, section_class, section_locator
+    add_element_name section_name
+    create_existence_checker section_name, section_locator
+    create_waiter section_name, section_locator
+    define_method section_name do
+      find section_locator
+    end
+  end
+
+  private
+
+  def create_waiter element_name, element_locator
+    method_name = "wait_for_#{element_name.to_s}"
+    if element_locator.nil?
+      define_method method_name do
+        raise SitePrism::NoLocatorForElement.new("#{self.class.name} => :#{element_name} needs a locator")
+      end
+    else
+      define_method method_name do |timeout|
+        if timeout == nil then timeout = Capybara.default_wait_time end
+        Capybara.using_wait_time timeout do
+          element_waiter element_locator
+        end
+      end
+    end
+  end
+end
