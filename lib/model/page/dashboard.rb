d9e87e40c797359aeee62fb6907d70506cbfa72f
Alistair Hutchison <alistair.hutchison@wildfireapp.com>
2012-05-22 14:06:20 +0100

    Fix failing successful login scenario

--- lib/model/page/dashboard.rb
+++ lib/model/page/dashboard.rb
@@ -1,13 +1,8 @@
 class Model::Page::Dashboard < SitePrism::Page
+  include ::Wildfire::Header
+  
   set_url "#{Helpers::Config['wildfire_site_root']}/dashboard/contests/"
   
   element :messenger_link, 'a.app_nav.messenger'
   element :account_button, ''
-  
-
-  section :dashboard_header, Model::Section::Generic::Header, 'div.head'
-
-  def logout
-
-  end
 end
