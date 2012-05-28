916295f6655ef8775255cfaa6f2f7b6bf5635107
Alistair Hutchison <alistair.hutchison@wildfireapp.com>
2012-05-28 17:51:29 +0100

    Step defs for adding Facebook properties

--- features/step_definitions/account_management/your_properties_steps.rb
+++ features/step_definitions/account_management/your_properties_steps.rb
@@ -8,13 +8,17 @@ Given /^I have added the twitter account "([^"]*)" to Wildfire$/ do |twitter_nam
   @wildfire.account_management.your_properties.add_twitter_property(twitter_name)
 end
 
-When /^I add the Facebook page "([^"]*)" to Wildfire$/ do |fb_page_name|
+When /^I (?:add|have added) the Facebook page "([^"]*)" to Wildfire$/ do |fb_page_name|
   @wildfire.account_management.load_section('Your Properties')
   @wildfire.account_management.your_properties.add_facebook_property(fb_page_name)
 end
 
 When /^I add the twitter account "([^"]*)" to Wildfire$/ do |twitter_name|
   @wildfire.account_management.your_properties.add_twitter_property(twitter_name)
+end
+
+When /^I remove the Facebook page "(.*?)" from Your Properties$/ do |page_name|
+  @wildfire.account_management.your_properties.remove_property(page_name)
 end
 
 Then /^I should see the following social properties:$/ do |table|
@@ -27,6 +31,10 @@ Then /^I should see Twitter account "([^"]*)" in Your Properties$/ do |twitter_n
   @wildfire.account_management.your_properties.should have_property(twitter_name.capitalize, 'twitter account')
 end
 
-Then /^I should see Facebook page "([^"]*)" in Your Properties$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^I should see Facebook page "([^"]*)" in Your Properties$/ do |page_name|
+  @wildfire.account_management.your_properties.should have_facebook_property(page_name)
+end
+
+Then /^I should have (\d+) social properties$/ do |count|
+  @wildfire.account_management.your_properties.social_properties.count.should == count.to_i
 end
--- lib/model/page/account_management/your_properties.rb
+++ lib/model/page/account_management/your_properties.rb
@@ -1,5 +1,7 @@
 class Model::Page::AccountManagement::YourProperties < SitePrism::Page
-  set_url_matcher /social_properties$/
+  include Helpers::ModalHelper
+  
+  set_url_matcher /social_networks$/
   
   element :social_properties_form, 'div.page_content form#social_properties'
   element :add_facebook_property_link, "div.page_content a:contains('Facebook')"
@@ -19,8 +21,13 @@ class Model::Page::AccountManagement::YourProperties < SitePrism::Page
     has_property?(name, 'twitter account')
   end
   
+  def has_facebook_property?(name)
+    has_property?(name, 'facebook page')
+  end
+  
   def has_properties?
-    using_wait_time(0.5) { has_content?("You haven't added any properties yet") ? false : true }
+    load unless displayed?
+    using_wait_time(2) { has_content?("You haven't added any properties yet") ? false : true }
   end
   
   def social_properties
@@ -32,32 +39,29 @@ class Model::Page::AccountManagement::YourProperties < SitePrism::Page
   
   def add_facebook_property(fb_page_name)
     add_facebook_property_link.click
-    within_facebook_properties_modal do
-      find("div:contains('#{fb_page_name}') input").check
+    within_modal do
+      find("#fan_page_tokens div.page:contains('#{fb_page_name}') input").set(true)
+      click_on('Add to Wildfire')
     end
+    wait_until() { !has_modal? }
   end
   
   def add_twitter_property(twitter_name)
     show_add_twitter_property_window
     within_window(page.driver.browser.window_handles.last){ Model::Page::TwitterOauth.new.authorise(twitter_name, 'w1ldf1r3')}
-    begin
-      wait_until() { has_twitter_property?(twitter_name.capitalize) || !flash_message.blank? }
-    rescue 
-      raise "Failed to add twitter property '#{twitter_name}\n#{flash_message}"
-    end
+    wait_until() { has_twitter_property?(twitter_name.capitalize) || !flash_message.blank? }
+  end
+  
+  def remove_property(name)
+    load unless displayed?
+    property_field = first("div.field:contains('#{name}')")
+    raise "Could not find property with name #{name} to remove" unless property_field
+    property_field.click_on('Remove')
+    page.accept_alert
   end
   
   def flash_message
     first('span.flash_contents').try(:text)
-  end
-  
-  private
-  def within_facebook_properties_modal(&block)
-    begin
-      # This currently raises Permission denied for <https://s-static.ak.facebook.com> to get property Window.frameElement (Selenium::WebDriver::Error::UnknownError)
-      # See http://code.google.com/p/selenium/issues/detail?id=2863 for details
-      within_frame(find("iframe[src*='fb/properties/new']")[:id]) { yield }
-    end
   end
   
   def show_add_twitter_property_window
