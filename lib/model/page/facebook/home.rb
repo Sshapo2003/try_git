ec6aed13cb609bf0b888d8f8d1e2df6b125d2421
Alistair Hutchison <alistair.hutchison@wildfireapp.com>
2012-06-21 13:15:30 +0100

    Fixes to the Facebook homepage model - should prevent failures on jenkins

--- lib/model/page/facebook/home.rb
+++ lib/model/page/facebook/home.rb
@@ -1,6 +1,9 @@
 class Model::Page::Facebook::Home < SitePrism::Page
   set_url 'http://www.facebook.com'
   
+  element :header_username, "span.headerTinymanName"
+  element :nav_dropdown_menu, "a#navAccountLink"
+  
   def login(email, password)
     load_as_logged_out
     fill_in('email', :with => email)
@@ -8,9 +11,17 @@ class Model::Page::Facebook::Home < SitePrism::Page
     click_on('Log in')
   end
   
+  def logout
+    nav_dropdown_menu.click
+    click_on 'Log out'
+  end
+  
+  def logged_in_user
+    has_header_username? ? header_username.text : nil
+  end
+  
   def load_as_logged_out
     load
-    Capybara.current_session.driver.browser.manage.delete_all_cookies
-    load
+    logout if logged_in_user
   end
 end
