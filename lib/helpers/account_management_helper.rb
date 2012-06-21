b52f9a22d6c4d40008597da5ebe0c4c34cda949c
Alistair Hutchison <alistair.hutchison@wildfireapp.com>
2012-06-21 12:39:11 +0100

    Fix the workaround for DE1278 to prevent certain account management scenarios from failing with a new account.

--- lib/helpers/account_management_helper.rb
+++ lib/helpers/account_management_helper.rb
@@ -1,13 +1,12 @@
 class Helpers::AccountManagementHelper
   class << self
     def register_and_login_new_user
-      # Workaround for Rally DE1278 - some apps error if we hit the signup page directly, so first
-      # visit the main front page to ensure we get cookies dropped
-      wildfire.front_page.load
-      wildfire.front_page.signup_link.click if ENV['CONFIG'] == 'am-test'
+      # Workaround for Rally DE1278 - some apps error if we hit the signup page directly, so
+      # ensure we follow the signup link from the login page
+      wildfire.login.load
+      wildfire.login.signup_link.click
       
       signup = wildfire.signup
-      signup.load
       signup.signup_register_user_form.complete_form_with_valid_credentials
       signup.signup_register_user_form.accept_terms
       signup.signup_register_user_form.submit_registration
