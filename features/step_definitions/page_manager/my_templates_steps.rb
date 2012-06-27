51ffd39cf0ebb7a8e536105715aedff13cd25a50
Nathan Bain <nathan_bain@hotmail.com>
2012-06-27 17:59:56 +0100

    Adding more resiliance to helpers for failing tests

--- features/step_definitions/page_manager/my_templates_steps.rb
+++ features/step_definitions/page_manager/my_templates_steps.rb
@@ -36,7 +36,7 @@ Given /^I have created a valid template$/ do
 end
 
 When /^I create a Blank Template$/ do
-  @wildfire.wildfireapp_page_manager.content_div.wait_for_templates
+  @wildfire.wildfireapp_page_manager.content_div.wait_for_templates(30)
   @num_templates_before = @wildfire.wildfireapp_page_manager.content_div.templates.count
   @template_name = Helpers::PageManagerHelper.create_template
 end
@@ -77,7 +77,7 @@ When /^I upload a new template$/ do
 end
 
 Given /^I have a valid uploaded template$/ do
-  @wildfire.wildfireapp_page_manager.content_div.wait_for_templates
+  @wildfire.wildfireapp_page_manager.content_div.wait_for_templates(30)
   @template = @wildfire.wildfireapp_page_manager.content_div.templates.select {|t| t.title_div.text.include? 'Uploaded Template'}.first
   @template_name = @template.title_div.text
 end
