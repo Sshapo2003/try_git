41eddc3d33efe7855342359160183f976797ee4f
Nathan Bain <nathan_bain@hotmail.com>
2012-06-27 17:50:12 +0100

    Adding more resiliance to helpers for failing tests

--- lib/helpers/page_manager_helper.rb
+++ lib/helpers/page_manager_helper.rb
@@ -14,7 +14,7 @@ class Helpers::PageManagerHelper
       wildfire.template_builder.edit_liquid_dialog_save_button.click
      
       sleep 1.0
-      wildfire.template_builder.wait_for_publish_template_changes_button
+      wildfire.template_builder.wait_for_publish_template_changes_button(30)
       wildfire.template_builder.page.execute_script("$('a.publish_template').click()")
 
       sleep 1.0
@@ -38,7 +38,7 @@ class Helpers::PageManagerHelper
 
       wildfire = Model::Wildfire.new
 
-      wildfire.wildfireapp_page_manager.wait_for_content_div
+      wildfire.wildfireapp_page_manager.wait_for_content_div(30)
       wildfire.wildfireapp_page_manager.content_div.get_template_by_title('Countdown Timer').wait_for_and_click_edit_link
       wildfire.wildfireapp_page_manager_edit_mode.sidebar.content_menu.wait_for_and_click_header_text_edit_link
       wildfire.wildfireapp_countdown_template_edit_header.body_text(updated_countdown_app_title)
@@ -50,8 +50,8 @@ class Helpers::PageManagerHelper
     def publish_completed_page
       wildfire = Model::Wildfire.new
 
-      wildfire.wildfireapp_page_manager_edit_mode.sidebar.publish_menu.wait_for_and_click_link
-      wildfire.wildfireapp_page_manager_edit_mode.sidebar.publish_menu.wait_for_and_click_publish_to_facebook_link
+      wildfire.wildfireapp_page_manager_edit_mode.sidebar.publish_menu.wait_for_and_click_link(30)
+      wildfire.wildfireapp_page_manager_edit_mode.sidebar.publish_menu.wait_for_and_click_publish_to_facebook_link(30)
 
       wildfire.wildfireapp_page_manager_edit_mode.publish_to_facebook_modal.wait_for_cancel_button(30)
 
@@ -68,7 +68,7 @@ class Helpers::PageManagerHelper
 
     def get_template_menu_options
       wildfire = Model::Wildfire.new
-      wildfire.wildfireapp_page_manager.content_div.templates[0].wait_for_drop_down_menu
+      wildfire.wildfireapp_page_manager.content_div.templates[0].wait_for_drop_down_menu(30)
       wildfire.wildfireapp_page_manager.content_div.templates[0].drop_down_menu.click
       wildfire.wildfireapp_page_manager.content_div.template_menu_options.collect {|o| o.text}
     end
@@ -79,7 +79,7 @@ class Helpers::PageManagerHelper
       wildfire.wildfireapp_page_manager.content_div.click_templates_menu_create_blank_template_option
 
       name = %{TestTemplate#{String.random}}
-      wildfire.wildfireapp_page_manager.new_template_dialog.wait_for_save_button
+      wildfire.wildfireapp_page_manager.new_template_dialog.wait_for_save_button(30)
       wildfire.wildfireapp_page_manager.new_template_dialog.template_name.set name
       wildfire.wildfireapp_page_manager.new_template_dialog.save_button.click
 
@@ -92,11 +92,11 @@ class Helpers::PageManagerHelper
       wildfire = Model::Wildfire.new
       content_div = wildfire.wildfireapp_page_manager.content_div
       template = content_div.get_template_by_title name
-      template.wait_for_drop_down_menu
+      template.wait_for_drop_down_menu(30)
       template.drop_down_menu.click
       link = template.template_menu_options.select {|o| o.text == "Clone Template" }.first[:href]
       content_div.page.execute_script %{ $('body > ol li a[href="#{link}"]').click() }
-      wildfire.wildfireapp_page_manager.wait_for_sticky_label
+      wildfire.wildfireapp_page_manager.wait_for_sticky_label(30)
       msg = "Unable to clone template. Sticky Message = #{wildfire.wildfireapp_page_manager.sticky_label.text}"
       Timeout.timeout_and_raise(30, msg) { sleep 0.1 while wildfire.wildfireapp_page_manager.sticky_label.text != 'You have successfully cloned the template.' }
     end
@@ -105,19 +105,19 @@ class Helpers::PageManagerHelper
       wildfire = Model::Wildfire.new
       content_div = wildfire.wildfireapp_page_manager.content_div
       template = content_div.get_template_by_title name
-      template.wait_for_drop_down_menu
+      template.wait_for_drop_down_menu(30)
       template.drop_down_menu.click
       link = template.template_menu_options.select {|o| o.text == "Edit Template Design" }.first[:href]
       template.visit link
 
-      wildfire.template_builder.wait_for_back_to_templates_button
+      wildfire.template_builder.wait_for_back_to_templates_button(30)
     end
 
     def edit_default_content(name='TestTemplate')
       wildfire = Model::Wildfire.new
       content_div = wildfire.wildfireapp_page_manager.content_div
       template = content_div.get_template_by_title name
-      template.wait_for_drop_down_menu
+      template.wait_for_drop_down_menu(30)
       template.drop_down_menu.click
       link = template.template_menu_options.select {|o| o.text == "Edit Default Content" }.first[:href]
       template.visit link
@@ -129,7 +129,7 @@ class Helpers::PageManagerHelper
       wildfire = Model::Wildfire.new
       content_div = wildfire.wildfireapp_page_manager.content_div
       template = content_div.get_template_by_title name
-      template.wait_for_drop_down_menu
+      template.wait_for_drop_down_menu(30)
       template.drop_down_menu.click
       link = template.template_menu_options.select {|o| o.text == "Delete Template" }.first[:href]
       content_div.page.execute_script %{ $('body > ol li a[href="#{link}"]').click() }
@@ -143,7 +143,7 @@ class Helpers::PageManagerHelper
     def upload_new_template
       wildfire = Model::Wildfire.new
       wildfire.wildfireapp_page_manager.content_div.upload_new_template_link.click
-      wildfire.upload_template.wait_for_template_name_textbox
+      wildfire.upload_template.wait_for_template_name_textbox(30)
 
       template_name = "Uploaded Template #{String.random}"
       wildfire.upload_template.template_name_textbox.set template_name
@@ -161,7 +161,7 @@ class Helpers::PageManagerHelper
       wildfire = Model::Wildfire.new
       content_div = wildfire.wildfireapp_page_manager.content_div
       template = content_div.get_template_by_title template_name
-      template.wait_for_drop_down_menu
+      template.wait_for_drop_down_menu(30)
       template.drop_down_menu.click
       link = template.template_menu_options.select {|o| o.text == "Upload New Version" }.first[:href]
       template.visit link
@@ -177,7 +177,7 @@ class Helpers::PageManagerHelper
       wildfire = Model::Wildfire.new
       content_div = wildfire.wildfireapp_page_manager.content_div
       template = content_div.get_template_by_title template_name
-      template.wait_for_drop_down_menu
+      template.wait_for_drop_down_menu(30)
       template.drop_down_menu.click
       link = template.template_menu_options.select {|o| o.text == "Download Template" }.first[:href]
     end
