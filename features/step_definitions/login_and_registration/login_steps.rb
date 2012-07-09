3e8cd9b6f413ab2a748421571daa7d6a71c36bf0
Alistair Hutchison <alistair.hutchison@wildfireapp.com>
2012-07-09 08:15:47 -0700

    Separate out step defs for new and existing user login

--- features/step_definitions/login_and_registration/login_steps.rb
+++ features/step_definitions/login_and_registration/login_steps.rb
@@ -1,12 +1,12 @@
-Given /^I am logged in to Wildfire as (.*)$/ do |user|
-  if user == 'a new user'
-    Helpers::AccountManagementHelper.register_and_login_new_user
-  else
-    credentials = Helpers::WildfireAccountsHelper.get_credentials_for(user)
-    raise "No credentials found for #{user} on environment #{ENV['CONFIG']}. Please add them to config/wildfire_users.yml." if credentials.blank?
-    @wildfire.login.load
-    @wildfire.login.login_user_credentials_form.login_with_credentials(credentials['email'], credentials['password'])
-  end
+Given /^I am logged in to Wildfire as ((?:!a new user).*)$/ do |user|
+  credentials = Helpers::WildfireAccountsHelper.get_credentials_for(user)
+  raise "No credentials found for #{user} on environment #{ENV['CONFIG']}. Please add them to config/wildfire_users.yml." if credentials.blank?
+  @wildfire.login.load
+  @wildfire.login.login_user_credentials_form.login_with_credentials(credentials['email'], credentials['password'])
+end
+
+Given /^I am logged in to Wildfire as a new user$/ do
+  Helpers::AccountManagementHelper.register_and_login_new_user
 end
 
 When /^I login to Wildfire as (.*)$/ do |user|
