a8cda9d2b12621ca1cb89b4af6104863003b379a
Nathan Bain <nathan_bain@hotmail.com>
2012-08-08 02:17:33 +0100

    Updates to model for uitk5

--- lib/model/section/messenger/uitk5_wildfireapp_messenger_compose_message_panel.rb
+++ lib/model/section/messenger/uitk5_wildfireapp_messenger_compose_message_panel.rb
@@ -1,5 +1,6 @@
 class Model::Section::Messenger::Uitk5WildfireappMessengerComposeMessagePanel < Model::Section::Messenger::WildfireappMessengerComposeMessagePanel
   element :send_button, "input[name='commit']"
   element :later_button, '.later input'
+  element :recepients_error, '#recipients .inline_error'
 
 end
