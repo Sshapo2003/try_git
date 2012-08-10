a8be4be52030353bf72a875ba27ddd4b9b867513
Alistair Hutchison <alistair.hutchison@wildfireapp.com>
2012-08-10 17:23:30 +0100

    Add missing file from previous commit

---
+++ lib/model/page/account_management/uitk5_create_company.rb
@@ -0,0 +1,14 @@
+class Model::Page::AccountManagement::Uitk5CreateCompany < SitePrism::Page
+  section :sidebar, Model::Section::Sidebar, '#sidebar'
+  
+  def load
+    sidebar.show_switchboard
+    sidebar.switchboard_panel.create_new_company
+  end
+  
+  def create_company(company_name, options={})
+    fill_in 'Company name', :with => company_name
+    select('Create New', :from => 'Account') if options[:new_account] == true
+    click_on 'Save'
+  end
+end
