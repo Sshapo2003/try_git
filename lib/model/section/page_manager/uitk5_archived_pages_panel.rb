1a7481e1638a2501a21d63e3742b5d58a9bc21ae
Nathan Bain <nathan_bain@hotmail.com>
2012-08-12 19:33:12 +0100

    Renaming due to misnaming of pages as templates

---
+++ lib/model/section/page_manager/uitk5_archived_pages_panel.rb
@@ -0,0 +1,3 @@
+class Model::Section::PageManager::Uitk5ArchivedPagesPanel < Model::Section::PageManager::Uitk5PagesPanel
+  sections :pages,  Model::Section::PageManager::Uitk5ArchivedPage, 'tbody tr'
+end
