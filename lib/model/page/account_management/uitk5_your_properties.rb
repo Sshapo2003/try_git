cd9aed02a46438fbd2194e13bb41e93f71dd96c5
Alistair Hutchison <alistair.hutchison@wildfireapp.com>
2012-08-14 10:42:48 +0100

    Handle Capybara slowness when adding Facebook properties

--- lib/model/page/account_management/uitk5_your_properties.rb
+++ lib/model/page/account_management/uitk5_your_properties.rb
@@ -9,13 +9,18 @@ class Model::Page::AccountManagement::Uitk5YourProperties < SitePrism::Page
   
   def add_facebook_property(fb_page_name)
     show_facebook_properties_modal
-    within_modal do
-      using_wait_time(60) do #Capybara seems slow at parsing long list of properties
+    begin
+      within_modal do
         find("tr:contains('#{fb_page_name}') input").set(true)
         click_on('Add to Wildfire')
       end
+    # Capybara is too slow when dealing with a user with hundreds of fb properties so fall back to using some javascript to add properties
+    rescue Timeout::Error => e
+      add_fb_property_with_javascript(fb_page_name)
+    rescue Capybara::TimeoutError => e
+      add_fb_property_with_javascript(fb_page_name)
     end
-    wait_until() { !has_modal? }
+    wait_until { !has_modal? }
   end
   
   def remove_property(name)
@@ -36,5 +41,11 @@ class Model::Page::AccountManagement::Uitk5YourProperties < SitePrism::Page
   def show_facebook_properties_modal
     add_property_button.click if has_add_property_button?
     fb_oauth_link.click
+  end
+  
+  private
+  
+  def add_fb_property_with_javascript(fb_page_name)
+    page.execute_script(%{$('.modal iframe').contents().find("tr:contains('#{fb_page_name}') input")[0].checked = true; $('.modal iframe').contents().find('.btn-primary').click();})
   end
 end
